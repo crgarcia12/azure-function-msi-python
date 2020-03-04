df0df3c94c2845a90055609142063dae5ddaa355
crgarcia12 <crgarcia@live.com.ar>
2020-03-04 14:31:43 +0100

    everything is broken

--- listnames/__init__.py
+++ listnames/__init__.py
@@ -2,23 +2,43 @@ import logging
 
 import azure.functions as func
 
+# Managed Service Identity (MSI) packages
+from msrestazure.azure_active_directory import MSIAuthentication
+from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
+
+# Sql driver
+import pyodbc
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    
+    # server = '<server>.database.windows.net'
+    # database = '<database>'
+    # username = '<username>'
+    # password = '<password>'
+    # driver= '{ODBC Driver 17 for SQL Server}'
+    # 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
+    connectionString = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:crgar-paas-msi-python-server.database.windows.net,1433;Database=crgar-paas-msi-python-db;Uid=sqladminuser;Pwd=P@ssword123123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
+    cnxn = pyodbc.connect(connectionString)
+    cursor = cnxn.curso
+    r()
+    cursor.execute("Select * From Names")
+    row = cursor.fetchone()
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
-    else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
-        )
+    allNames=""
+    while row:
+        allNames += str(row[0]) + ", "
+        row = cursor.fetchone()
+
+    return func.HttpResponse(f"Hello {allNames}!")
+
+
+
+# token = b"eyJ0eXAiOi...";
+# exptoken = b"";
+# for i in token:
+#     exptoken += bytes({i});
+#     exptoken += bytes(1);
+# tokenstruct = struct.pack("=i", len(exptoken)) + exptoken;
+# conn = pyodbc.connect(connstr, attrs_before = { 1256:tokenstruct });
