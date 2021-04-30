b6db341c3b09072c6a7a69469bd0c8710afe1182
AutoExtractor <crgarcia@live.com.ar>
2021-04-30 11:45:47 +0200

    using latest msi library

--- listnames/__init__.py
+++ listnames/__init__.py
@@ -1,44 +1,42 @@
 import logging
-
 import azure.functions as func
-
-# Managed Service Identity (MSI) packages
-from msrestazure.azure_active_directory import MSIAuthentication
-from azure.mgmt.resource import ResourceManagementClient, SubscriptionClient
 
 # Sql driver
 import pyodbc
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
 
-    
-    # server = '<server>.database.windows.net'
-    # database = '<database>'
-    # username = '<username>'
-    # password = '<password>'
-    # driver= '{ODBC Driver 17 for SQL Server}'
-    # 'DRIVER='+driver+';SERVER='+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password
-    connectionString = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:crgar-paas-msi-python-server.database.windows.net,1433;Database=crgar-paas-msi-python-db;Uid=sqladminuser;Pwd=P@ssword123123;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
-    cnxn = pyodbc.connect(connectionString)
-    cursor = cnxn.curso
-    r()
-    cursor.execute("Select * From Names")
-    row = cursor.fetchone()
+    try:
 
-    allNames=""
-    while row:
-        allNames += str(row[0]) + ", "
-        row = cursor.fetchone()
+        logging.info('Python HTTP trigger function processed a request.')
 
-    return func.HttpResponse(f"Hello {allNames}!")
+        # Connecting to Azure SQl the standard way
+        server = 'tcp:crgar-paas-msi-python-sqlserver.database.windows.net' 
+        database = 'crgar-paas-msi-python-db' 
+        driver = '{ODBC Driver 13 for SQL Server}'
 
+        with pyodbc.connect(
+            "Driver="
+            + driver
+            + ";Server="
+            + server
+            + ";PORT=1433;Database="
+            + database
+            + ";Authentication=ActiveDirectoryMsi",
+        ) as conn:
 
+            logging.info("Successful connection to database")
 
-# token = b"eyJ0eXAiOi...";
-# exptoken = b"";
-# for i in token:
-#     exptoken += bytes({i});
-#     exptoken += bytes(1);
-# tokenstruct = struct.pack("=i", len(exptoken)) + exptoken;
-# conn = pyodbc.connect(connstr, attrs_before = { 1256:tokenstruct });
+            with conn.cursor() as cursor:
+                #Sample select query
+                cursor.execute("SELECT * FROM People;") 
+
+                peopleNames = 'Names: '
+                row = cursor.fetchone() 
+                while row: 
+                    peopleNames += str(row[0]) + ', ' 
+                    row = cursor.fetchone()
+
+                return func.HttpResponse(f"Hello {peopleNames}!")
+    except Exception as e:
+        return func.HttpResponse(e)
